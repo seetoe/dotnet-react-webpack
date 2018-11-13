7aa0d515957fc041da82ccd863cd552b96d272a4
ego-esukhomlyn <esukhomlyn@ego-cms.com>
2018-11-13 14:56:32 +0200

    Some formatting

--- Startup.cs
+++ Startup.cs
@@ -11,35 +11,33 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 namespace webpacktest
 {
     public class Startup
-{
-// This method gets called by the runtime. Use this method to add services to the container.
-// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-public void ConfigureServices(IServiceCollection services)
-{
-services.AddMvc();
-}
-// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-{
-if (env.IsDevelopment())
-{
-app.UseDeveloperExceptionPage();
-app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-      {
-         HotModuleReplacement = true
-      });
-}
-app.UseStaticFiles();
-app.UseMvc(routes => {
-routes.MapRoute(
-name: "default",
-template: "{controller=Home}/{action=Index}/{id?}"
-);
-});
-app.Run(async (context) =>
-{
-await context.Response.WriteAsync("Hello World!");
-});
-}
-}
+    {
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+            {
+                HotModuleReplacement = true
+            });
+        }
+        app.UseStaticFiles();
+        app.UseMvc(routes => {
+            routes.MapRoute(
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}"
+            );
+        });
+        app.Run(async (context) =>
+        {
+            await context.Response.WriteAsync("Hello World!");
+        });
+        }
+    }
 }
